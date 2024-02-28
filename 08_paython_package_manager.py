555ef4e3ef4c542cbe412b039535891d0474a421
Antonimapla <tonimapla@gmail.com>
2024-02-28 23:08:57 +0100

    package y backend

--- 08_paython_package_manager.py
+++ 08_paython_package_manager.py
@@ -13,11 +13,17 @@ print(numpy_array * 2)
 
 import pandas # pip install pandas
 import requests
-
+"""
 response = requests.get("https://pokeapi.co/api/v2/pokemon?limit=151")
 print(response)
 print(response.status_code)
 print(response.json())
+"""
+# Arithmetics package
+
+from mypackage import arithmetics
+
+print(arithmetics.sum_two_values(1, 4))
 
 
 
---
+++ backend/type_hints.py
@@ -0,0 +1,5 @@
+### type hints ###
+
+my_string_variable = "My String variable"
+print(my_string_variable)
+print(type(my_string_variable))
