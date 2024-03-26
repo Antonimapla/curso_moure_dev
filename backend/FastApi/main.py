d2f3698574e491261d7fa2167773991a68076f00
Antonimapla <tonimapla@gmail.com>
2024-03-26 23:11:07 +0100

    fastapi

--- backend/FastApi/main.py
+++ backend/FastApi/main.py
@@ -2,10 +2,21 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+# Url local: http://127.0.0.1:8000
+
 @app.get("/")
 async def root():
     return "¡Hola FastAPI!"
 
+
+# Url local: http://127.0.0.1:8000/url
+
 @app.get("/url")
 async def url():
     return {"url":"https://mouredev.com/python"} 
+
+#inicia el server: uvicorn main:app --reload
+#detener el servidor: CTRL+C
+
+#documentacion con Swagger: http://127.0.0.1:8000/docs
+#documentacion con Redocly: http://127.0.0.1:8000/redoc
